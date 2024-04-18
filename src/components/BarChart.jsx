4535ef00cbd5172bf65c1f112cd5467bf33d769e
tephaMR22 <stephania.martinez.rodriguez@correounivalle.edu.co>
2024-04-17 23:27:13 -0500

    Add loadings

--- src/components/BarChart.jsx
+++ src/components/BarChart.jsx
@@ -108,6 +108,7 @@ const valueFormatter = (value) => `${value}%`;
 
 export default function BarsDataset() {
     const [loading, setLoading] = useState(true); // Estado para controlar la carga
+    const [errorMessage, setErrorMessage] = useState(null); // Estado para almacenar el mensaje de error
     const [data, setData] = useState(null); // Estado para almacenar los datos
 
     useEffect(() => {
@@ -121,7 +122,9 @@ export default function BarsDataset() {
         }).then((response) => {
 
             if (response.data.message) {
-                return <p> {response.data.message} </p>
+                setErrorMessage(response.data.message);
+                setLoading(false); // Cambia el estado de carga a false
+                return;
             }
             console.log(response)
             const emotionsData = response.data.emotions_totals;
@@ -149,9 +152,57 @@ export default function BarsDataset() {
     }, []); // Ejecuta el efecto solo una vez al montar el componente
 
     if (loading) {
-        return <p>Tu información se está cargando...</p>;
+        return <div className='flex flex-col justify-center items-center'>
+                    <svg height="108px" width="108px" viewBox="0 0 128 128" className="loader">
+                        <defs>
+                            <clipPath id="loader-eyes">
+                                <circle transform="rotate(-40,64,64) translate(0,-56)" r="8" cy="64" cx="64" className="loader__eye1"></circle>
+                                <circle transform="rotate(40,64,64) translate(0,-56)" r="8" cy="64" cx="64" className="loader__eye2"></circle>
+                            </clipPath>
+                            <linearGradient y2="1" x2="0" y1="0" x1="0" id="loader-grad">
+                                <stop stopColor="#000" offset="0%"></stop>
+                                <stop stopColor="#fff" offset="100%"></stop>
+                            </linearGradient>
+                            <mask id="loader-mask">
+                                <rect fill="url(#loader-grad)" height="128" width="128" y="0" x="0"></rect>
+                            </mask>
+                        </defs>
+                        <g strokeDasharray="175.93 351.86" strokeWidth="12" strokeLinecap="round">
+                            <g>
+                                <rect clipPath="url(#loader-eyes)" height="64" width="128" fill="hsl(193,90%,50%)"></rect>
+                                <g stroke="hsl(193,90%,50%)" fill="none">
+                                    <circle transform="rotate(180,64,64)" r="56" cy="64" cx="64" className="loader__mouth1"></circle>
+                                    <circle transform="rotate(0,64,64)" r="56" cy="64" cx="64" className="loader__mouth2"></circle>
+                                </g>
+                            </g>
+                            <g mask="url(#loader-mask)">
+                                <rect clipPath="url(#loader-eyes)" height="64" width="128" fill="hsl(223,90%,50%)"></rect>
+                                <g stroke="hsl(223,90%,50%)" fill="none">
+                                    <circle transform="rotate(180,64,64)" r="56" cy="64" cx="64" className="loader__mouth1"></circle>
+                                    <circle transform="rotate(0,64,64)" r="56" cy="64" cx="64" className="loader__mouth2"></circle>
+                                </g>
+                            </g>
+                        </g>
+                    </svg>
+                    <p className='text-white mt-1'>Tu información se está cargando...</p>
+                </div>
+        
     }
 
+    if (errorMessage) {
+        // return ;
+        return <div className='flex flex-col justify-center items-center'>
+            <p className='text-white m-3'>{errorMessage}</p>
+            <span className="loader-eye"></span>
+        </div>
+
+    }
+
+    // Si no hay datos ni mensaje de error, no renderiza nada más
+    if (!data) {
+        return null;
+    }
+    
     return (
         <div className=' dark:border-slate-600 border-slate-300  border rounded-md shadow-lg backdrop-filter backdrop-blur-sm bg-opacity-15 bg-black m-4 p-4 w-2/3'>
             <h2 className='dark:text-white text-xl ml-2 mb-2 font-bold tracking-wider'>Resumen de la semana</h2>
